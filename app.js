ded315d25ebba1a28151515b44a40b7fe0cd5548
lolochka <ooshkoda@gmail.com>
2015-11-21 14:31:57 +0200

    Lecture 21

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var person = {
-	firstname: 'John',
-	lastname: 'Doe',
-	greet: function () {
-		console.log('Hello ' + this.firstname + ' ' + this.lastname);	
-	}
+//pass bu value
+function change(b) {// b here only copy of value A (b = a)
+	//b = 2; (b = 2)
+	console.log(a);//a
+	// in this case we add A only as a value, we cant change it
+	//but in case
+	a = 2 + b; // a = 2 + b
+	console.log(a); //a
+	//we change our var A and use b as argument equal A
 };
 
-person.greet();
+var a = 1;
+change(a);
+console.log(a);
 
-person['greet']();
-console.log(person['firstname']);
+//pass by reference
+function changeObj(d) {
+	d.prop = 'val';//change objectd added as argument
+}
+
+var obj = {
+	name: 'obj',
+	num: 13
+}
+changeObj(obj);
+console.log(obj);
+console.log(obj.prop);
